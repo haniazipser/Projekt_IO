9667c2d0cf4f1cb7ba7955dc1fd6bf8bfd92568d
Hania <haniazipser2004@gmail.com>
2025-05-13 20:25:38 +0200

    Changed Sets to Lists In courseDto -lessonTimes, -lessons InlessonDto -exercises

--- Backend/src/main/kotlin/com/example/Projekt_IO/Model/Dtos/CourseDto.java
+++ Backend/src/main/kotlin/com/example/Projekt_IO/Model/Dtos/CourseDto.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -18,8 +20,8 @@ public class CourseDto {
     private String name;
     private String  instructor;
     private String creator;
-    private Set<LessonTime> lessonTimes;
-    private Set<LessonDto> lessons;
+    private List<LessonTime> lessonTimes;
+    private List<LessonDto> lessons;
     private Instant startDate;
     private Instant endDate;
     private Integer frequency;
@@ -29,9 +31,9 @@ public class CourseDto {
         this.name = course.getName();
         this.instructor = course.getInstructor();
         this.creator = course.getCreator();
-        this.lessonTimes = course.getLessonTimes();
+        this.lessonTimes = course.getLessonTimes().stream().toList();
         if (course.getLessons() != null) {
-            this.lessons = course.getLessons().stream().map(s -> new LessonDto(s)).collect(Collectors.toSet());
+            this.lessons = course.getLessons().stream().map(s -> new LessonDto(s)).sorted(Comparator.comparing(LessonDto::getClassDate)).collect(Collectors.toList());
         }
         this.endDate = course.getEndDate();
         this.startDate = course.getStartDate();
