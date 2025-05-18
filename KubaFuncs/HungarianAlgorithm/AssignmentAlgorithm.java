aa8c655bd894e9d812cfe92284dfc0ef870fa6c6
Jnowak123 <jacksokoleoko@gmail.com>
2025-05-18 14:24:55 +0200

    fixed overflow issue

--- KubaFuncs/HungarianAlgorithm/AssignmentAlgorithm.java
+++ KubaFuncs/HungarianAlgorithm/AssignmentAlgorithm.java
@@ -22,7 +22,8 @@ public class AssignmentAlgorithm {
         if (randomizeValue > 0) {
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
-                    matrix[i][j] += Math.random() * randomizeValue;
+                    if (matrix[i][j] != Double.MAX_VALUE) {
+                        matrix[i][j] += Math.random() * randomizeValue;
                 }
             }
         }
